f2d06480dbf7bea84cbf0a351528be33d4b20e6a
DenisLazarian <denis.emanuel.lazarian@gmail.com>
2024-04-12 17:23:46 +0200

    Realizando el examen parcial 1 2016/17.

--- src/parcial1_2016_17/Pokedex.java
+++ src/parcial1_2016_17/Pokedex.java
@@ -1,2 +1,55 @@
-package parcial1_2016_17;public class Pokedex {
+package parcial1_2016_17;
+
+import acm.program.CommandLineProgram;
+import acm.program.ConsoleProgram;
+
+import java.util.StringTokenizer;
+
+public class Pokedex extends CommandLineProgram {
+
+    RangeSet[] generations = new RangeSet[7];
+
+    public void run() {
+        addGenerations();
+        String pokemonData = "2#12 5#6 2#7 2#12 3#4 5#6 3#2 1#12 6#4";
+//        String pokemonData = readLine("List of tokens: ");
+        processSets(pokemonData);
+        showResults();
+    }
+
+    private void showResults() {
+        String s = "";
+        for (int i = 0; i < generations.length; i++) {
+            int np = generations[i].size();
+            if(np>0) s = s.concat("Generation "+ i+ ": "+ np + "\n");
+        }
+
+        RangeSet ru = RangeSet.unionAll(generations);
+        s = s.concat("Dorsales diferentes: "+ ru.size());
+        println(s);
+    }
+
+    private void processSets(String p) {
+        StringTokenizer tk = new StringTokenizer(p, " ");
+        while(tk.hasMoreTokens()){
+            String item = tk.nextToken();
+            StringTokenizer tk2 = new StringTokenizer(item, "#");
+
+            while(tk2.hasMoreTokens()){
+                int generation = Integer.parseInt(tk2.nextToken());
+                int pokemon = Integer.parseInt(tk2.nextToken());
+                generations[generation].add(pokemon);
+            }
+        }
+    }
+
+    private void addGenerations(){
+        for (int i = 0; i < generations.length; i++) {
+            generations[i] = new RangeSet(1,100);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Pokedex().start(args);
+    }
 }
--- src/parcial1_2016_17/RangeSet.java
+++ src/parcial1_2016_17/RangeSet.java
@@ -1,2 +1,72 @@
-package parcial1_2016_17;public class RangeSet {
+package parcial1_2016_17;
+
+import org.w3c.dom.ranges.Range;
+
+public class RangeSet {
+    private int min;
+    private int max;
+    private int[] interval;
+    private boolean zeroAdded;
+
+    public RangeSet(int min, int max){
+        interval = new int[max-min];
+        this.min = min;
+        this.max = max;
+        zeroAdded = false;
+    }
+
+    boolean add(int elem){
+        if(isOutOfBound(elem)) return false;
+
+        if(elem == 0) zeroAdded = true;
+
+        interval[elem - min] = elem;
+        return true;
+    }
+
+    boolean contains(int elem){
+        return !isOutOfBound(elem) && interval[elem - min] != 0;
+    }
+
+    private boolean includeZero(){ // ya tengo garantizado que max siempre es mayor o igual a min.
+        return min>0 || max<0;
+    }
+
+    public static RangeSet union(RangeSet rs1, RangeSet rs2){
+        int min = Math.min(rs1.min, rs2.min);
+        int max = Math.max(rs1.max, rs2.max);
+
+        RangeSet result = new RangeSet(min, max);
+
+        for (int i = min; i < max; i++) {
+            if(rs1.contains(i)) {
+                result.add(i);
+            } else if (rs2.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public int size(){
+        int c = 0;
+        for (int i = 0; i < interval.length; i++) {
+            if (this.contains(i)) c++;
+        }
+        if(includeZero() && zeroAdded) c++; // si el conjunto incluye un espacio para 0 y además lo hemos añadido, aumentamos en 1.
+        return c;
+    }
+
+    private boolean isOutOfBound(int elem){
+        return elem >= max || elem < min;
+    }
+
+    public static RangeSet unionAll(RangeSet[] set){
+        RangeSet result = set[0];
+        for (int i = 1; i < set.length; i++) {
+            result = RangeSet.union(result, set[i]);
+        }
+        return result;
+    }
+
 }
